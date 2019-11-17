eff212bd0d039c9dfca71e56940ea0a434cd1b77
rama naidu <mramachandra810@gmail.com>
2019-11-17 10:05:58 +0530

    Smaple updates in function

---
+++ FirstProject/SampleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SampleViewController.swift
+//  FirstProject
+//
+//  Created by ramachandra on 17/11/19.
+//  Copyright © 2019 ramachandra. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func sayHello() {
+        print("Hello Ram")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var secVC = SampleViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    func sayHello() {
+        print("Say Hellohttps://github.com/Ramachandra-Naidu/FirstProject.git")
     }
 
 
